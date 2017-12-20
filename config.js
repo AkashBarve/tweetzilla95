382c934169a03e02556359123e11dd65803d1e60
Akash <akashbarve0511@gmail.com>
2017-12-20 17:30:38 -0500

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@
  * access_token_secret
  */
 module.exports = {
-  consumer_key: '3odin3Pot8CFmzAU1UEDHOOQW',  
-  consumer_secret: 'viSXyvaa0AmQGjVrwclUHTTzKgbLziFIK7UUB1QSwdJq24GQQy',
-  access_token: '942895284237033479-S5A9T2twnmPLmLfb4lTPsTEjYZusmzO',  
-  access_token_secret: 'DvwE7is5H4K5ajpGwCb5PyMpR0Av80YxV4160oAm7BrWX'
+  consumer_key: 'enter you own',  
+  consumer_secret: 'enter you own',
+  access_token: 'enter you own',  
+  access_token_secret: 'enter you own'
 }
